c54f0a75a7418c8e6b09241425468e51262ecacc
Passionate-Koda <banjimayowa1@gamil.com>
2017-08-17 17:17:34 +0100

    adds test

---
+++ server/specs.js
@@ -0,0 +1,26 @@
+var request = require('supertest'),
+    chai    = require('chai'),
+    app     = require('./server.js'),
+    assert  = chai.assert,
+    expect  = chai.expect,
+    should  = chai.should();
+
+describe("#todo", function(){
+  it("should insert a todo item", function(done){
+    var todo = {
+      task: "do some testing",
+      date: "23rd Aug, 2017",
+      time: "2pm"
+    }
+
+    request(app)
+    .post('/api/v1/todos')
+    .send(todo)
+    .end(function(err, res){
+      expect(res.body).to.be.an("Object");
+      assert(res.body.time, "5pm")
+
+      done();
+    })
+  })
+})
